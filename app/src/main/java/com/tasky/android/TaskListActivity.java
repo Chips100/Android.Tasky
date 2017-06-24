357acf21f6ad96e2817099f2a79f701fb451f9a3
Dennis Janiak <dennis.janiak@googlemail.com>
2017-06-24 18:58:01 +0200

    Provide Snackbar after setting a task to done that allows reverting the action.

--- app/src/main/java/com/tasky/android/TaskListActivity.java
+++ app/src/main/java/com/tasky/android/TaskListActivity.java
@@ -2,6 +2,7 @@ package com.tasky.android;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -20,6 +21,8 @@ import org.joda.time.DateTime;
 
 public class TaskListActivity extends AppCompatActivity {
     private TaskManager taskManager;
+    private Snackbar revertTaskDoneSnackbar;
+    private Task lastTaskDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +38,8 @@ public class TaskListActivity extends AppCompatActivity {
             createNewTask();
             }
         });
+
+        revertTaskDoneSnackbar = setupRevertTaskDoneSnackbar(fab);
 
         // TODO: As a starting point, we do very poor man's DI and Entourage Pattern all the way.
         // Would be cool to change it. But that needs more understanding...
@@ -68,6 +73,8 @@ public class TaskListActivity extends AppCompatActivity {
      * Displays the UI to create a new task.
      */
     public void createNewTask() {
+        hideSnackbar();
+
         new CreateTaskDialog(this, new CreateTaskDialog.OnTaskCreateListener() {
             @Override
             public boolean handleTaskCreate(String title, DateTime dueDate) {
@@ -86,14 +93,52 @@ public class TaskListActivity extends AppCompatActivity {
         TaskArrayAdapter adapter = new TaskArrayAdapter(this, new TaskArrayAdapter.OnTaskDoneListener() {
             @Override
             public void handleTaskDone(Task task) {
-                // Set task to done via manager and reload the relevant task list accordingly.
-                taskManager.setTaskDone(task.getId());
-                renderRelevantTasks();
+                setTaskDone(task);
             }
         });
         adapter.addAll(taskManager.getRelevantTasks());
 
         ListView relevantTaskList = (ListView)findViewById(R.id.relevantTaskList);
         relevantTaskList.setAdapter(adapter);
+    }
+
+    /**
+     * Sets the done-state of the specified task.
+     * @param task Task that should be done.
+     */
+    public void setTaskDone(Task task) {
+        // Set task to done via manager and reload the relevant task list accordingly.
+        taskManager.setTaskDone(task.getId());
+        renderRelevantTasks();
+
+        // Present an option to revert the action.
+        lastTaskDone = task;
+        revertTaskDoneSnackbar.show();
+    }
+
+    /**
+     * Creates a Snackbar that allows to revert setting the last task to done.
+     * @param view View to find a parent from.
+     * @return The created Snackbar.
+     */
+    private Snackbar setupRevertTaskDoneSnackbar(View view) {
+        return Snackbar.make(view, R.string.task_done_message, Snackbar.LENGTH_INDEFINITE)
+            .setAction(R.string.undo, new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (lastTaskDone == null) return;
+
+                    taskManager.revertTaskDone(lastTaskDone.getId());
+                    lastTaskDone = null;
+                    renderRelevantTasks();
+                }
+            });
+    }
+
+    /**
+     * Hides any Snackbar that might currently be displayed.
+     */
+    public void hideSnackbar() {
+        revertTaskDoneSnackbar.dismiss();
     }
 }
